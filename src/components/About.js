ad98f61a4cb3b543ce889babef0b6ce3d4241a2e
lakendrafarris <62860234+lakendrafarris@users.noreply.github.com>
2020-08-03 19:17:40 -0500

    updated built with

--- src/components/About.js
+++ src/components/About.js
@@ -16,10 +16,10 @@ export default class About extends Component {
                     <div className="banner-text"> 
                     <div className="boxed">
                         <p>
-                            I am an enthusiastic software engineer attending Digital Crafts Houston to be completed in July 2020
+                            I am an enthusiastic software engineer that graduated from Digital Crafts Houston in July 2020 
                             and is eager to join the technology industry. As I am an Army veteran, I enjoy being challenged and engaged with projects that require me to work 
                             outside my comfort and knowledge set while working alone or in a collaborative environment. 
-                            My passion for creating practical proficient software is what drives me to keep striving to deliver stellar work                                 ethic and drive. I am a skilled professional with proven leadership and supervisory skills, outstanding communication and interpersonal skills, 
+                            My passion for creating practical proficient software is what drives me to keep striving to deliver stellar work ethic and drive. I am a skilled professional with proven leadership and supervisory skills, outstanding communication and interpersonal skills, 
                             and a friendly, pleasant demeanor with excellent social skills to encourage a healthy atmosphere. 
                         </p>  
                         </div> 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,18 +15,20 @@ export default class Portfolio extends Component {
                       <div id='portfolio-wrapper' className='bgrid-quarters s-bgrid-thirds cf'>
                           <div className='content'>
                               <div className='item-wrap'>
-                              <a href='https://github.com/DjangoKerger/HtownAPP'>
+                              <a href='https://github.com/lakendrafarris/HtownAPP'>
                                       <img src='imgs/htown.png' alt='htown'/></a>
                                       {/* <ExternalLink href="https://github.com/lakendrafarris"> <img src='imgs/htown.png' alt='htown'/></ExternalLink> */}
                                       {/* <img src='imgs/htown.png' alt='htown'/> */}
                                   <div className='overlay'>
                                       <div className='portfolio-item-meta'>
                                           <h5>HTOWN App</h5>
+                                          
                                           {/* <a href={Htownapp}>HTown App</a> 
                                           <Button href="https://github.com/lakendrafarris">Link</Button>  */}
 
 
                                           <p>We designed this full stack application because Houston is very, very large. We wanted everyone to have the chance to explore what Houstonians think about their own neighborhoods. This app allows users to rate their neighborhood based on four properties: school system, entertainment scene, outdoor recreation, and home prices. After rating, users must register and login in order to make a comment and discuss their neighborhood with others.</p>
+                                          <p>Built with : Javascript, Node.js, Bootstrap, Css, Mustache, AWS, PostgresSQL, Sequelize, Express, npm</p>
                                       </div>
                                   </div>
                               </div>
@@ -40,6 +42,7 @@ export default class Portfolio extends Component {
                                           <h5>My Recipe Hampers</h5>
                                           <a href='http://cumbersome-art.surge.sh/'>My Recipe Hampers</a>
                                           <p>Simple react application that allows users to view, create, delete and update recipes.</p>
+                                          <p>Built with: HTML, CSS3, Javascript, Bootstrap, Third party Api, Node.js, Express.js, PostresSQL,and ElephantSQL</p>
                                       </div>
                                   </div>
                               </div>
